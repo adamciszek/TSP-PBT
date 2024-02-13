ee18afcd621a64cc1b4ed8cf9f036f4ae7a884f8
adamciszek <ciszek.adam914@gmail.com>
2024-02-13 11:34:34 -0500

    update to TSPProperties.java

--- src/test/java/tspTest/TSPProperties.java
+++ src/test/java/tspTest/TSPProperties.java
@@ -2,8 +2,11 @@ package TSPTest;
 
 import TSP.*;
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
+import net.jqwik.api.constraints.NotEmpty;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.data.*;
+import org.junit.jupiter.api.Test;
 
 
 class TSPProperties {
@@ -11,7 +14,7 @@ class TSPProperties {
     // TESTING City.java ===================
     @Property
     @Report(Reporting.GENERATED)
-    void cityProperties(@ForAll String name, @ForAll int ID, @ForAll boolean visited) {
+    void cityProperties(@ForAll @NotEmpty String name, @ForAll @IntRange(min=0, max=100) int ID, @ForAll boolean visited) {
         City city = new City(name, ID, visited);
 
         // verify the properties set by the constructor
@@ -19,26 +22,31 @@ class TSPProperties {
         Assertions.assertThat(city.getID()).isEqualTo(ID);
         Assertions.assertThat(city.isVisited()).isEqualTo(visited);
 
-        String newName = "NewCityName";
-        int newID = 42;
+        String newName = "NewCity";
+        int newID = -10;
         boolean newVisited = !visited;
 
         city.setName(newName);
         city.setID(newID);
         city.setVisited(newVisited);
 
-        // verify that mutator methods change the properties
+        // verify the properties changed
         Assertions.assertThat(city.getName()).isEqualTo(newName);
         Assertions.assertThat(city.getID()).isEqualTo(newID);
         Assertions.assertThat(city.isVisited()).isEqualTo(newVisited);
     }
 
-
     @Property
     @Report(Reporting.GENERATED)
-    void toStringMethodProducesValidOutput(@ForAll String name, @ForAll int ID, @ForAll boolean visited) {
+    void toStringMethodProducesValidOutput(@ForAll @NotEmpty String name, @ForAll @IntRange(min=0, max=100) int ID, @ForAll boolean visited) {
         City city = new City(name, ID, visited);
         String expectedOutput = "City{name=" + name + ", ID=" + ID + ", visited=" + visited + '}';
         Assertions.assertThat(city.toString()).isEqualTo(expectedOutput);
     }
+
+    // TESTING Route.java
+
+
+
+    // TESTING TSP.java
 }
