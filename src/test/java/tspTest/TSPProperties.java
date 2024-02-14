46c853eea3d7b01e2583b69dc030f3d106dfc2bb
adamciszek <ciszek.adam914@gmail.com>
2024-02-13 19:21:22 -0500

    added testing for city and route classes

--- src/test/java/tspTest/TSPProperties.java
+++ src/test/java/tspTest/TSPProperties.java
@@ -1,20 +1,37 @@
 package TSPTest;
 
-import TSP.*;
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.CharRange;
 import net.jqwik.api.constraints.IntRange;
 import net.jqwik.api.constraints.NotEmpty;
+import net.jqwik.api.constraints.StringLength;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.data.*;
 import org.junit.jupiter.api.Test;
 
+import TSP.City;
+import TSP.Route;
 
-class TSPProperties {
+import java.util.List;
 
-    // TESTING City.java ===================
+public class TSPProperties {
+
+    private static final City VANCOUVER = new City("Vancouver", 0, false);
+    private static final City EDMONTON = new City("Edmonton", 1, false);
+    private static final City CALGARY = new City("Calgary", 2, false);
+    private static final City WINNIPEG = new City("Winnipeg", 3, false);
+    private static final City HAMILTON = new City("Hamilton", 4, false);
+    private static final City TORONTO = new City("Toronto", 5, false);
+    private static final City KINGSTON = new City("Kingston", 6, false);
+    private static final City OTTAWA = new City("Ottawa", 7, false);
+    private static final City MONTREAL = new City("Montreal", 8, false);
+    private static final City HALIFAX = new City("Halifax", 9, false);
+
+    // TESTING City.java ============================================================================
+
     @Property
     @Report(Reporting.GENERATED)
-    void cityProperties(@ForAll @NotEmpty String name, @ForAll @IntRange(min=0, max=100) int ID, @ForAll boolean visited) {
+    void cityProperties(@ForAll @CharRange(from='A', to='Z') @StringLength(min=1,max = 2) String name, @ForAll @IntRange(max=2) int ID, @ForAll boolean visited) {
         City city = new City(name, ID, visited);
 
         // verify the properties set by the constructor
@@ -38,15 +55,54 @@ class TSPProperties {
 
     @Property
     @Report(Reporting.GENERATED)
-    void toStringMethodProducesValidOutput(@ForAll @NotEmpty String name, @ForAll @IntRange(min=0, max=100) int ID, @ForAll boolean visited) {
+    void toStringMethodProducesValidOutput(@ForAll @CharRange(from='A', to='Z') @StringLength(min=1,max = 2) String name, @ForAll @IntRange(max=2) int ID, @ForAll boolean visited) {
         City city = new City(name, ID, visited);
         String expectedOutput = "City{name=" + name + ", ID=" + ID + ", visited=" + visited + '}';
         Assertions.assertThat(city.toString()).isEqualTo(expectedOutput);
     }
 
-    // TESTING Route.java
+    // TESTING Route.java ===============================================================================================
+    @Property
+    @Report(Reporting.GENERATED)
+    void routePropertiesWithStartCity(@ForAll("validCities") City startCity) {
+        Route route = new Route(startCity);
 
+        Assertions.assertThat(route.getStartCity()).isEqualTo(startCity);
+        Assertions.assertThat(route.getCurrentCity()).isEqualTo(startCity);
+        Assertions.assertThat(route.getRoute()).containsExactly(startCity);
+    }
 
+    @Property
+    @Report(Reporting.GENERATED)
+    void setCurrentCityShouldChangeCurrentCity(@ForAll("validCities") City startCity,
+                                               @ForAll("validCities") City newCity) {
+        Route route = new Route(startCity);
+        route.setCurrentCity(newCity);
 
-    // TESTING TSP.java
+        Assertions.assertThat(route.getCurrentCity()).isEqualTo(newCity);
+    }
+
+    @Property
+    @Report(Reporting.GENERATED)
+    void setRouteShouldChangeRouteList(@ForAll("validCities") City startCity,
+                                       @ForAll("cityList") List<City> newRoute) {
+        Route route = new Route(startCity);
+        route.setRoute(newRoute);
+
+        Assertions.assertThat(route.getRoute()).isEqualTo(newRoute);
+    }
+
+    // custom generator for valid cities
+    @Provide
+    Arbitrary<City> validCities() {
+        return Arbitraries.of(VANCOUVER, EDMONTON, CALGARY, WINNIPEG, HAMILTON,
+                TORONTO, KINGSTON, OTTAWA, MONTREAL, HALIFAX);
+    }
+    @Provide
+    Arbitrary<List<City>> cityList() {
+        return validCities().list().ofMinSize(0).ofMaxSize(10);
+    }
+
+    // TESTING TSP.java ===============================================================================================
+
 }
